b9e7282a34435370f00ad5c662a3fe4f11242fe2
Manuel Marquez <buzondemam@gmail.com>
2018-03-26 17:51:38 -0400

    [ADD] purchase_lines_no_order: Eliminates the order on the purchase order lines.

---
+++ purchase_lines_no_order/README.md
@@ -0,0 +1,5 @@
+No order in Purchase Orders Lines
+=================================
+
+Eliminates the order on the purchase order lines.
+
---
+++ purchase_lines_no_order/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Uakami - Manuel Marquez <buzondemam@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'No Order in Purchase Order Lines',
+    'version': '11.0.0.1.0',
+    'category': 'Purchase',
+    'author': 'Uakami',
+    'website': "https://uakami.com/",
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/purchase_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
